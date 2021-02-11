88970165bc498c3d991711fdb0ac3b3d6575bca9
trofimowova <trofimowova@yandex.ru>
2021-02-11 02:37:24 -0800

    Lesson4.Fix

--- authnapp/admin.py
+++ authnapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import ShopUser
+
+
+class ShopUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "age"),
+            },
+        ),
+    )
+
+
+admin.site.register(ShopUser, ShopUserAdmin)
