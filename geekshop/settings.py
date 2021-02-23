156592554da26bc8756fdc72b85cc2cd3cb02f45
trofimowova <trofimowova@yandex.ru>
2021-02-23 12:42:12 -0800

    lesson_7. CRUD categories.

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -30,16 +30,16 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'mainapp',
-    'authnapp',
-    'basketapp',
-    'adminapp',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "mainapp",
+    "authnapp",
+    "basketapp",
+    "adminapp",
 ]
 
 # Auth model
