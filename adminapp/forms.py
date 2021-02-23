788212ead223e4e1b46090132a5b929ac8c18b50
trofimowova <trofimowova@yandex.ru>
2021-02-23 12:15:21 -0800

    lesson_7. CRUD users.

---
+++ adminapp/forms.py
@@ -0,0 +1,9 @@
+from authnapp.forms import ShopUserEditForm
+from authnapp.models import ShopUser
+
+
+class ShopUserAdminEditForm(ShopUserEditForm):
+    class Meta:
+        model = ShopUser
+        fields = "__all__"
+        
