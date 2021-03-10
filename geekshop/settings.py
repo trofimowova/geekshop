49f17f4e339b8ef97408561bb19a546ba578334d
trofimowova <trofimowova@yandex.ru>
2021-03-10 02:30:57 -0800

    lesson_11. Ordersapp

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "basketapp",
     "adminapp",
     "social_django",
+    "ordersapp",
 ]
 
 # Auth model
@@ -74,6 +75,7 @@ TEMPLATES = [
                 "mainapp.context_processors.basket",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
+                "django.template.context_processors.media",
             ],
         },
     },
